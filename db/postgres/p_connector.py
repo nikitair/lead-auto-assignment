47111757aebbbc12a765cd3f35532c20c258b2ed
Nikita <stlrvnk@gmail.com>
2023-12-07 11:56:46 +0200

    postgres connector

---
+++ db/postgres/p_connector.py
@@ -0,0 +1,39 @@
+import os
+import logging
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def postgres_connector(func):
+    def inner(*args, **kwargs):
+        conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER,
+                                password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT)
+        result = func(conn, *args, **kwargs)
+        conn.close()
+        return result
+    return inner
+
+
+@postgres_connector
+def demo_query(connector):
+    curr = connector.cursor()
+    curr.execute("SELECT * FROM demo")
+    data = curr.fetchall()
+    curr.close()
+    logging.info(data)
+    return data
+
+
+if __name__ == "__main__":
+    demo_query()
