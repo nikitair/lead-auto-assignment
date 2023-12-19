86aa4f9f9b0b43503ab38bead706f13f112a9880
Nikita <stlrvnk@gmail.com>
2023-12-19 17:38:21 +0200

    error displayment

--- server.py
+++ server.py
@@ -21,26 +21,26 @@ def lead_auto_assignment():
     """
     lead auto assignment endpoint
     """
-    try:
-        # receiving lead payload
-        payload = request.get_json()
-        logging.info(f"PAYLOAD RECEIVED -- {pprint.pformat(payload)}\n")
+    # try:
+    # receiving lead payload
+    payload = request.get_json()
+    logging.info(f"PAYLOAD RECEIVED -- {pprint.pformat(payload)}\n")
 
-        # extracting useful information from the payload
-        postalcode = payload.get("listing_zip") if payload.get("listing_zip") != "N/A" else ""
-        listing_province = payload.get("listing_province") if payload.get("listing_province") != "N/A" else ""
-        listing_city = payload.get("listing_city") if payload.get("listing_city") != "N/A" else ""
-        buyer_city = payload.get("buyer_city") if payload.get("buyer_city") != "N/A" else ""
-        buyer_province = payload.get("buyer_province") if payload.get("buyer_province") != "N/A" else ""
+    # extracting useful information from the payload
+    postalcode = payload.get("listing_zip") if payload.get("listing_zip") != "N/A" else ""
+    listing_province = payload.get("listing_province") if payload.get("listing_province") != "N/A" else ""
+    listing_city = payload.get("listing_city") if payload.get("listing_city") != "N/A" else ""
+    buyer_city = payload.get("buyer_city") if payload.get("buyer_city") != "N/A" else ""
+    buyer_province = payload.get("buyer_province") if payload.get("buyer_province") != "N/A" else ""
 
-        # executing lead auto assignment function; returning result
-        result = main(postalcode, listing_province, listing_city, buyer_city, buyer_province)
-        return jsonify(result), 200
+    # executing lead auto assignment function; returning result
+    result = main(postalcode, listing_province, listing_city, buyer_city, buyer_province)
+    return jsonify(result), 200
     
-    except Exception as e:
-        error_message = {"status": "fail", "error": "Bad request", "details": str(e)}
-        logging.error(f"\n!!! SERVER ERROR OCCURRED -- {str(e)}\n")
-        return jsonify(error_message), 400
+    # except Exception as e:
+    #     error_message = {"status": "fail", "error": "Bad request", "details": str(e)}
+    #     logging.error(f"\n!!! SERVER ERROR OCCURRED -- {str(e)}\n")
+    #     return jsonify(error_message), 400
 
 
 app = WSGIMiddleware(app)
