97d21225a11a85fc696526fe52edd91fb4b09a28
Nikita <stlrvnk@gmail.com>
2024-01-10 12:58:31 +0200

    postgres connector with SSH

--- db/postgres/p_connector.py
+++ db/postgres/p_connector.py
@@ -3,7 +3,7 @@ from logging_config import logger as  logging
 import pretty_errors
 from dotenv import load_dotenv
 import psycopg2
-import paramiko
+from sshtunnel import SSHTunnelForwarder
 
 load_dotenv()
 
@@ -13,44 +13,12 @@ POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
-
-# SSH Tunnel configuration
-POSTGRES_SSH_HOST = os.getenv("POSTGRES_SSH_HOST")
-POSTGRES_SSH_PORT = os.getenv("POSTGRES_SSH_PORT")
-POSTGRES_SSH_USERNAME = os.getenv("POSTGRES_SSH_USERNAME")
-POSTGRES_SSH_PASSWORD = os.getenv("POSTGRES_SSH_PASSWORD")
-
-
-# logging.basicConfig(level=logging.DEBUG,
-#                     format='%(asctime)s - %(levelname)s - %(message)s')
-
-def create_ssh_tunnel():
-    """
-    Create an SSH tunnel to the PostgreSQL server
-    """
-    ssh_client = paramiko.SSHClient()
-    ssh_client.load_system_host_keys()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(
-        POSTGRES_SSH_HOST,
-        POSTGRES_SSH_PORT,
-        POSTGRES_SSH_USERNAME,
-        POSTGRES_SSH_PASSWORD
-    )
-
-    # Choose a local port for the tunnel
-    local_port = 5432
-
-    # Create an SSH tunnel to the PostgreSQL server
-    remote_bind_address = (POSTGRES_HOST, int(POSTGRES_PORT))
-    ssh_tunnel = ssh_client.get_transport().open_channel(
-        'direct-tcpip', ('127.0.0.1', local_port), remote_bind_address
-    )
-
-    # Close the SSH connection (tunnel remains open)
-    ssh_client.close()
-
-    return local_port
+SSH_MODE = int(os.getenv("SSH_MODE"))
+SSH_PKEY = os.getenv("SSH_PKEY")
+SSH_USERNAME = os.getenv("SSH_USERNAME")
+SSH_SERVER_ADDRESS=os.getenv("SSH_SERVER_ADDRESS")
+SSH_SERVER_PORT = int(os.getenv("SSH_SERVER_PORT"))
+LOCAL_PORT = os.getenv("LOCAL_PORT")
 
 
 def postgres_connector(func):
@@ -59,18 +27,26 @@ def postgres_connector(func):
     """
     def inner(*args, **kwargs):
 
-        # SSH connection
-        # local_port = create_ssh_tunnel() 
-        # conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER,
-        #                         password=POSTGRES_PASSWORD, host='127.0.0.1', port=local_port)
+        if SSH_MODE == 1:
+            # starting SSH tunnel
+            server = SSHTunnelForwarder(
+                (SSH_SERVER_ADDRESS, SSH_SERVER_PORT),
+                ssh_username=SSH_USERNAME,
+                ssh_pkey=SSH_PKEY,
+                remote_bind_address=(POSTGRES_HOST, POSTGRES_PORT),
+                local_bind_address=("localhost", LOCAL_PORT)
+            )
+            server.start()
+            logging.info("POSTGRES SSH TUNNEL STARTED")
+
+            conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER,
+                                    password=POSTGRES_PASSWORD, host="localhost", port=LOCAL_PORT)
+        else:
+             conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER,
+                                    password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT)   
         
 
-        # usual connection
-        conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER,
-                                password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT)
-        
-
-        logging.info("POSTGRES CONNECTED")
+        logging.info(f"POSTGRES CONNECTED WITH SSH MODE {SSH_MODE}")
         try:
             return func(conn, *args, **kwargs)
         except Exception as ex:
@@ -78,6 +54,9 @@ def postgres_connector(func):
         finally:
             conn.close()
             logging.info("POSTGRES DISCONNECTED")
+            if SSH_MODE == 1:
+                server.stop()
+                logging.info("POSTGRES SSH TUNNEL DISCONNECTED")
 
     return inner
 
