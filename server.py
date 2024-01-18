c5d05aeb25c7c6b3695c26dcc0f67d9448bf98f4
Nikita <stlrvnk@gmail.com>
2024-01-18 18:15:30 +0200

    server logging improvement

--- server.py
+++ server.py
@@ -26,7 +26,8 @@ def lead_auto_assignment():
     """
     # receiving lead payload
     payload = request.get_json()
-    logging.info(f"\n\n{lead_auto_assignment.__name__} -- RAW PAYLOAD -- {payload}")
+    logging.info(f"\n\n{lead_auto_assignment.__name__} -- LEAD AUTO ASSIGNMENT TRIGGERED")
+    logging.info(f"{lead_auto_assignment.__name__} -- RAW PAYLOAD -- {payload}")
 
     # extracting useful information from the payload
     postalcode = payload.get("listing_zip") if payload.get(
