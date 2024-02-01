5a5af55fd353cfc84ce13f08a6c5ebf3d168c39d
Nikita <stlrvnk@gmail.com>
2024-02-01 17:32:03 +0200

    moved from src

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/olegl/retool_processor/lead_auto_assignment_automation/.venv/bin/activate
-cd /home/olegl/retool_processor/lead_auto_assignment_automation/src
+cd /home/olegl/retool_processor/lead_auto_assignment_automation
 daphne server:app -p 5000 -b 172.31.26.132
