698fdaf402ed02ef2b189955b44451ec622eb3f6
Nikita <stlrvnk@gmail.com>
2024-02-01 17:30:56 +0200

    5000 port in run sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/olegl/retool_processor/lead_auto_assignment_automation/.venv/bin/activate
 cd /home/olegl/retool_processor/lead_auto_assignment_automation/src
-daphne server:app -p 5050 -b 172.31.26.132
+daphne server:app -p 5000 -b 172.31.26.132
